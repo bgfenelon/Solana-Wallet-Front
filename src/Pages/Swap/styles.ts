3e889b194b2439a714d2129cb45de2ac30bf0404
vitor-sld <vitor.cteles@gmail.com>
2025-12-01 16:00:42 -0300

    ajustes finais

--- src/Pages/Swap/styles.ts
+++ src/Pages/Swap/styles.ts
@@ -6,7 +6,10 @@ export const Container = styled.div`
   width: 100%;
   padding: 40px;
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
+  gap: 100px;
 `;
 
 export const Box = styled.div`
